cfd5080c20a60ce3ce2480ee8e83dd22435c9c15
student <Muthoniyahya@gmail.com>
2021-03-21 21:50:41 +0300

    first commit

---
+++ Iman.js
@@ -0,0 +1,22 @@
+function getGrade(){
+var marks=74;
+if(marks>=80){
+console.log('A');
+
+}else if(marks>=74){
+console.log('B');
+
+}else if(marks>=68){
+    console.log('C');
+
+}else if(marks>=59){
+    console.log('D');
+
+}else if(marks>=49){
+    console.log('E');
+
+}else{
+    console.log('F');
+}
+}
+getGrade();
---
+++ java.js
@@ -0,0 +1,7 @@
+var age=18;
+if(age>0){
+    console.log("You are a minor");
+    let newAge=age*10;
+    console.log(newAge);
+}
+console.log(age);
